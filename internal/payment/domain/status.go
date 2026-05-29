a973fb82087d3cbb8aaa6e2c602f60d9251aa7ca
sungjun-ever <zkfmak10@gmail.com>
2026-05-29 23:39:07 +0900

    기본 모델 셋팅

---
+++ internal/payment/domain/status.go
@@ -0,0 +1,12 @@
+package domain
+
+type PaymentStatus string
+
+const (
+	PaymentStatusPending   PaymentStatus = "PENDING"
+	PaymentStatusRequested PaymentStatus = "REQUESTED"
+	PaymentStatusSucceeded PaymentStatus = "SUCCEEDED"
+	PaymentStatusFailed    PaymentStatus = "FAILED"
+	PaymentStatusCanceled  PaymentStatus = "CANCELED"
+	PaymentStatusRefunded  PaymentStatus = "REFUNDED"
+)
