6f46d081369ea4f6d8e2e878a36907b9407660cb
sungjun-ever <zkfmak10@gmail.com>
2026-05-30 15:32:01 +0900

    /cmd/api/main.go -> internal/bootstrap/app.go 를 불러와 실행하는 것으로 변경 실제 config 설정, db 의존성, router 등 조립은 boostrap 부분으로 넘김
    database/mysql.go
    redis/redis.go
    각 db 연결 설정

---
+++ internal/boostrap/app.go
@@ -0,0 +1,61 @@
+package boostrap
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"payment_system/internal/config"
+	"payment_system/internal/database"
+	"payment_system/internal/redis"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type App struct {
+	Router *gin.Engine
+}
+
+func NewApp() *App {
+	cfg := config.Load()
+
+	_ = database.NewMysql(cfg)
+	_ = redis.NewRedis(cfg)
+
+	router := NewRouter()
+
+	return &App{
+		Router: router,
+	}
+}
+
+func (app *App) Run() {
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: app.Router.Handler(),
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+
+	log.Println("Server exiting")
+}
