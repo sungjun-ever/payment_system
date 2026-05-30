da44ad5c91c2f20cf68b1158334b372e03317680
sungjun-ever <zkfmak10@gmail.com>
2026-05-30 16:16:04 +0900

    에러 미들웨어에서 사용할 실패 응답 정의

---
+++ internal/common/response/fail.go
@@ -0,0 +1,27 @@
+package response
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type ErrorInfo struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+}
+
+type FailResponse struct {
+	Success bool       `json:"success,default:false"`
+	Error   *ErrorInfo `json:"error"`
+}
+
+func ToResponse(c *gin.Context, status int, code, message string) {
+	response := FailResponse{
+		Success: false,
+		Error: &ErrorInfo{
+			Code:    code,
+			Message: message,
+		},
+	}
+
+	c.JSON(status, response)
+}
