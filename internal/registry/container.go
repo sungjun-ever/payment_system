34f14e58627619531b181de328f5cd9895591e43
sungjun-ever <zkfmak10@gmail.com>
2026-06-01 14:49:21 +0900

    container 및 router 수정
    1. container에 user_handler 주입
    2. router에서 user create 요청 맵핑

--- internal/registry/container.go
+++ internal/registry/container.go
@@ -2,24 +2,39 @@ package registry
 
 import (
 	"log/slog"
-	"payment_system/internal/database"
+	"payment_system/internal/handler"
 	"payment_system/internal/redis"
+	"payment_system/internal/repository"
+	"payment_system/internal/service"
+
+	"gorm.io/gorm"
 )
 
 type Container struct {
-	Logger *slog.Logger
-	Mysql  *database.Mysql
-	Rds    *redis.Redis
+	Logger      *slog.Logger
+	Mysql       *gorm.DB
+	Rds         *redis.Redis
+	UserHandler *handler.UserHandler
 }
 
 func NewContainer(
 	logger *slog.Logger,
-	mysql *database.Mysql,
+	mysql *gorm.DB,
 	rds *redis.Redis,
 ) *Container {
+	// repo
+	userRepo := repository.NewUserRepository(mysql)
+
+	// svc
+	userSvc := service.NewUserService(userRepo)
+
+	// handler
+	userHandler := handler.NewUserHandler(userSvc)
+
 	return &Container{
-		Logger: logger,
-		Mysql:  mysql,
-		Rds:    rds,
+		Logger:      logger,
+		Mysql:       mysql,
+		Rds:         rds,
+		UserHandler: userHandler,
 	}
 }
