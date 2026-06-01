f49f23368d49b23fb50081c9c95fbd67e59ab4fe
sungjun-ever <zkfmak10@gmail.com>
2026-06-01 14:48:25 +0900

    user 관련 CSR 구현 1. user_repository, user_service, user_handler 2. 사용자 생성 기능 추가

---
+++ internal/handler/user_handler.go
@@ -0,0 +1,37 @@
+package handler
+
+import (
+	"payment_system/internal/common/errUtils"
+	"payment_system/internal/common/response"
+	"payment_system/internal/dto/user"
+	"payment_system/internal/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+type UserHandler struct {
+	us *service.UserService
+}
+
+func NewUserHandler(us *service.UserService) *UserHandler {
+	return &UserHandler{us}
+}
+
+func (u *UserHandler) Create(c *gin.Context) {
+	ctx := c.Request.Context()
+	var req user.CreateRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		_ = c.Error(errUtils.NewApiError(errUtils.LevelError, 400, errUtils.C001, "유효하지 않은 입력값", nil))
+		return
+	}
+
+	createdUser, err := u.us.CreateUser(ctx, req)
+
+	if err != nil {
+		_ = c.Error(err)
+		return
+	}
+
+	response.ToSuccessResponse(c, 201, user.NewResource(createdUser))
+}
