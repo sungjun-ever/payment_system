2833236cf7114f23408289c00faa590070931496
sungjun-ever <zkfmak10@gmail.com>
2026-06-04 17:45:37 +0900

    handler: error_mapper.go 생성 handler 계층 apperr 변환 통일

--- internal/handler/auth_handler.go
+++ internal/handler/auth_handler.go
@@ -109,23 +109,3 @@ func (a *AuthHandler) Refresh(c *gin.Context) {
 
 	response.ToSuccessResponse(c, 200, authDto.NewResource(tokens.AccessToken))
 }
-
-func toAppError(err error) *apperr.AppError {
-	switch {
-	case errors.Is(err, service.ErrInvalidToken):
-		return apperr.NewAppError(apperr.LevelWarn, 401, apperr.A002, err, nil)
-
-	case errors.Is(err, service.ErrTokenNotFound):
-		return apperr.NewAppError(apperr.LevelInfo, 401, apperr.A003, err, nil)
-
-	case errors.Is(err, service.ErrInvalidCredentials):
-		return apperr.NewAppError(apperr.LevelInfo, 401, apperr.C001, err, nil)
-
-	case errors.Is(err, service.ErrTokenConflict):
-		return apperr.NewAppError(apperr.LevelWarn, 409, apperr.A002, err, nil)
-
-	default:
-		return apperr.NewAppError(apperr.LevelError, 500, apperr.S001, err, nil)
-	}
-
-}
