458bddacb7bc161cb44cd92b1d1ed0e7686cf611
sungjun-ever <zkfmak10@gmail.com>
2026-06-05 22:05:21 +0900

    에러 로그 미들웨어 변경적용 1. 파일명 변경, 메스드명 변경 2. 에러 레벨별 별도 로그레벨 사용하도록 변경

---
+++ internal/middleware/error_handler.go
@@ -0,0 +1,47 @@
+package middleware
+
+import (
+	"context"
+	"errors"
+	"log/slog"
+	"payment_system/internal/pkg/apperr"
+	"payment_system/internal/pkg/response"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ErrorHandlerMiddleware(log *slog.Logger) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Next()
+
+		if len(c.Errors) > 0 {
+			err := c.Errors.Last().Err
+			var appError *apperr.AppError
+
+			requestID, _ := c.Get("request_id")
+
+			errorLogByLevel(log, c.Request.Context(), err, appError, requestID.(string))
+
+			if errors.As(err, &appError) {
+				response.ToFailResponse(c, appError.Status, appError.Code, appError.Code.String())
+			} else {
+				response.ToFailResponse(c, 500, apperr.S001, "Internal Server Error")
+			}
+		}
+	}
+}
+
+func errorLogByLevel(log *slog.Logger, ctx context.Context, err error, appError *apperr.AppError, requestID string) {
+	if errors.As(err, &appError) {
+		switch appError.Level {
+		case apperr.LevelError:
+			log.ErrorContext(ctx, "Request Failed", slog.String("error", err.Error()), slog.String("request_id", requestID))
+		case apperr.LevelWarn:
+			log.WarnContext(ctx, "Request Failed", slog.String("error", err.Error()), slog.String("request_id", requestID))
+		default:
+			log.InfoContext(ctx, "Request Failed", slog.String("error", err.Error()), slog.String("request_id", requestID))
+		}
+	} else {
+		log.ErrorContext(ctx, "Request Failed", slog.String("error", err.Error()), slog.String("request_id", requestID))
+	}
+}
