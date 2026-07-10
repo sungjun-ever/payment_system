06bf93e3a4b0ab95d2646edc349787081c8954ac
sungjun-ever <zkfmak10@gmail.com>
2026-07-10 14:01:41 +0900

    PaymentStatus -> PaidStatus, RefundStatus 분리

--- internal/payment/domain/payment.go
+++ internal/payment/domain/payment.go
@@ -6,22 +6,28 @@ import (
 	"gorm.io/gorm"
 )
 
-type PaymentStatus string
+type PaidStatus string
+type RefundStatus string
 
 const (
-	Processing PaymentStatus = "PROCESSING"
-	Succeeded  PaymentStatus = "SUCCEEDED"
-	Rejected   PaymentStatus = "REJECTED"
-	Failed     PaymentStatus = "FAILED"
+	PaidProcessing PaidStatus = "PROCESSING"
+	PaidSucceeded  PaidStatus = "SUCCEEDED"
+	PaidRejected   PaidStatus = "REJECTED"
+	PaidFailed     PaidStatus = "FAILED"
+
+	RefundProcessing RefundStatus = "PROCESSING"
+	RefundSucceeded  RefundStatus = "SUCCEEDED"
+	RefundRejected   RefundStatus = "REJECTED"
+	RefundFailed     RefundStatus = "FAILED"
 )
 
 type Payment struct {
 	gorm.Model
 
-	UserID    uint          `gorm:"not null;index;column:user_id"`
-	PaymentNo string        `gorm:"type:varchar(50);not null;uniqueIndex:uk_payments_payment_no;column:payment_no"`
-	OrderID   uint          `gorm:"not null;uniqueIndex:uk_payments_order_id;column:order_id"`
-	Status    PaymentStatus `gorm:"type:varchar(30);not null;index;column:status"`
+	UserID    uint       `gorm:"not null;index;column:user_id"`
+	PaymentNo string     `gorm:"type:varchar(50);not null;uniqueIndex:uk_payments_payment_no;column:payment_no"`
+	OrderID   uint       `gorm:"not null;uniqueIndex:uk_payments_order_id;column:order_id"`
+	Status    PaidStatus `gorm:"type:varchar(30);not null;index;column:status"`
 
 	PaidAt     *time.Time `gorm:"column:paid_at"`
 	CanceledAt *time.Time `gorm:"column:canceled_at"`
